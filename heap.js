d3f68ddeb89f5b2060f95631bee522a6d6fae820
Saksham Lodha <legitsaksham@gmail.com>
2025-10-29 22:44:53 +0530

    stack nd heaps

---
+++ heap.js
@@ -0,0 +1,12 @@
+// heap are basicaly used in non primitive datatype..function array ...
+// // reference value is being used
+// ----------------------|
+//                       |
+//                       |
+//                       |
+//                       |
+//                       |
+//                       |
+//                       |
+// ----------------------|
+// this bo is repping heap
