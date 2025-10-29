d72898612b6153f3f630adc3beaa772ed246179c
Saksham Lodha <legitsaksham@gmail.com>
2025-10-30 03:35:16 +0530

    IIFE done

---
+++ IIFe.js
@@ -0,0 +1,11 @@
+// immediately invoked function expression(IIFE)
+(function chai(){
+    console.log(`db connected`);
+    
+})()
+
+// ()()-->this is the syntax for iife
+(function printing()
+    {
+        console.log("saksham");
+})();
