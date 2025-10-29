7eb5b75a714c472736efc8e2c7ad3658bdcf93a0
Saksham Lodha <legitsaksham@gmail.com>
2025-10-30 02:36:16 +0530

    obj done...

---
+++ obj_destructure.js
@@ -0,0 +1,10 @@
+// destructuring
+const course = {
+    corusename:"js in hindi",
+    price:"519",
+    courseowner:"Inflcit"
+}
+//course.courseowner
+const {courseowner:malik} = course;
+console.log(malik);
+
