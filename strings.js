514d4f4033e46c1a94f3b5506e6908703b1d6802
Saksham Lodha <legitsaksham@gmail.com>
2025-10-29 23:03:51 +0530

    string basics done

---
+++ strings.js
@@ -0,0 +1,17 @@
+const hero = "Akshay";
+const movie = " Phir Hera Pheri ";
+console.log(hero+ " Best Movie is = " +movie);
+
+// interpolation or backticks (` `)
+console.log(`Best Movie is : ${movie}`);
+console.log(`Best Hero is : ${hero}`);
+
+// declaring string using object 
+const gameName= new String("Inflict Codes");
+console.log(gameName);
+console.log(typeof gameName);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('c'));
