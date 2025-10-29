112567ad5da3049e18cbf40eff9bf2c3a51c51e2
Saksham Lodha <legitsaksham@gmail.com>
2025-10-29 23:51:06 +0530

    math done...

---
+++ mathandnumber.js
@@ -0,0 +1,12 @@
+// its a by-default lib that comes with js
+
+console.log(Math);
+console.log(Math.abs(-1));//give absolute val
+console.log(Math.round(5.999));//give round off number 
+console.log(Math.ceil(4.0001));//ceil- top val - 4.0001 = 5
+console.log(Math.floor(4.0001));//floor- bottom val - 4.0001 = 4
+// -----important RANDOM -----
+console.log(Math.random());//random generate random values and it can vary on basis of range but by default in 0 to 1
+console.log((Math.random()*10)+1);
+// wrapping wioth math.floor
+console.log(Math.floor(Math.random()*10)+1);
