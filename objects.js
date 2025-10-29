a2fe09e42f72c7c3fe800beeff8daf8d32e3ea40
Saksham Lodha <legitsaksham@gmail.com>
2025-10-30 02:11:27 +0530

    object/dict part 1done..

---
+++ objects.js
@@ -0,0 +1,26 @@
+// two way of declaration - object,literal
+// singleton - apne tareeke ka ek he object hai , hamesha constructor se banega
+
+// Object Literals
+// Object.create this is the way of constructor and always singleton
+// Object Literals
+const jsUser = {
+  Name: "saksham",
+  "full": "lodha",
+  Alive: true
+};
+// console.log(jsUser);
+
+// Object.freeze(jsUser)
+jsUser.Name = "LordInflict";
+
+// console.log(jsUser.Name);
+// console.log(jsUser.Alive);
+// console.log(jsUser["full"]);
+
+jsUser.greeting = function() { 
+  console.log(`Hello jsuser ${this.Name}`);
+};
+
+console.log(jsUser.greeting()); // This just shows the function definition
+// console.log(jsUser.greeting());
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,3 @@
-const marvel_heros =["spideman","Cap","Hawkeye"]
-// const dc_heros =["superman","flash","batmnan"]
-// // marvel_heros.push(dc_heros)
-// console.log (marvel_heros);
-// // console.log (marvel_heros[3][1]
+
+// console.log(jsUser.Alive);
+// console.log(jsUser["full"]
