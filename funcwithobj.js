a83a83b0e4e09fd7522c1b0ece7c399873ae9b26
Saksham Lodha <legitsaksham@gmail.com>
2025-10-30 02:57:26 +0530

    function with objects

---
+++ funcwithobj.js
@@ -0,0 +1,6 @@
+// here the three fullstop work as rest opr making all values come thru one var name and those three dots are aka spread opr for concat
+function calccartprice(...num1){
+
+    return num1
+}
+console.log(calccartprice(200,400,600));
