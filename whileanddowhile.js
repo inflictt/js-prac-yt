1a5c58aec8924c057e20a2376832e29313acf0ef
Saksham Lodha <legitsaksham@gmail.com>
2025-10-30 11:42:21 +0530

    while and do while done

---
+++ whileanddowhile.js
@@ -0,0 +1,13 @@
+let index= 0
+while(index===0){
+    console.log("yes the conditon is true");
+    break;
+    index++;
+    
+}
+// do while
+do {
+    
+    console.log("Index value is: ",index);
+    index++;
+} while (index==0);
