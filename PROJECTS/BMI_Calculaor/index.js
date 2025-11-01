177ddbce907e8da431a941b1851b681b8dd1cc3c
Saksham Lodha <legitsaksham@gmail.com>
2025-11-02 02:43:07 +0530

    BMI calc done

---
+++ PROJECTS/BMI_Calculaor/index.js
@@ -0,0 +1,20 @@
+// here we will talk about form and submit event
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const result = document.querySelector('#result');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = 'Please give a valid height';
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = 'Please give a valid weight';
+  } else {
+  const bmi = (weight * 10000) / (height * height);
+    // show bmi in result
+    results.innerHTML = `<span>${bmi.toFixed(1)}<span>`;
+  }
+});
